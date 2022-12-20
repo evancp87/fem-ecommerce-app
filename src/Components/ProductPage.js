1812c14475f49bb233cc0a303e251ff68aa3ad31
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-12-20 22:58:25 +0000

    removed redundant Slider component

--- src/Components/ProductPage.js
+++ src/Components/ProductPage.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ProductDescription from "./ProductDescription";
 import Lightbox from "./Lightbox";
-import Slider from "./Slider";
 
 function ProductPage() {
   return (
     <main className="product-page">
       <Lightbox />
-      <Slider />
       <ProductDescription />
     </main>
   );
