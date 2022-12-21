d640c78567e60de87adbbcbbc67f1ac07d82e146
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-12-21 23:35:23 +0000

    removed sidebar component for insertion into nav as conditional with hamburger buttons

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProductPage from "./Components/ProductPage";
 import Nav from "./Components/Nav";
-import Sidebar from "./Components/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Sidebar />
         <Nav />
         <Routes>
           <Route path="/" element={<ProductPage />}></Route>
